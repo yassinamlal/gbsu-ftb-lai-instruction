e3fef38d6e26bdf6e927eee235333db8f77c0623
yassinamlal <amlalyassin@gmail.com>
2022-02-17 22:33:00 +0100

    ajout fct
    ajout fct

--- gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
+++ gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
@@ -5,8 +5,61 @@ import org.springframework.stereotype.Component
 @Component
 class GbsuFtbLaiService {
 
+    companion object {
+        const val divTrois = 3
+        const val divCinq = 5
+        const val Gbsu = "Gbsu"
+        const val Ftb = "Ftb"
+        const val Lai = "Lai"
+
+        const val trois = '3'
+        const val cinq = '5'
+        const val sept = '7'
+    }
+
     fun convertNumber(inputNumber: Int): String {
-        return ""
+
+        var value: StringBuilder = StringBuilder();
+        var isMatch: Boolean = false
+        var stringNumbur = inputNumber.toString();
+
+        //If the number is divisible by 3
+        (inputNumber % divTrois == 0)
+        if (inputNumber % divTrois == 0) {
+            value.append(Gbsu);
+            isMatch = true;
+        }
+        //If the number is divisible by 5
+        if (inputNumber % divCinq == 0) {
+            value.append(Ftb);
+            isMatch = true;
+        }
+
+        //if the number contains 3, 5, 7
+        if (stringNumbur.contains(trois) || stringNumbur.contains(cinq) || stringNumbur.contains(sept)) {
+
+            value = containsNumber(stringNumbur, value)
+            isMatch = true;
+        }
+        
+        if (isMatch) {
+            return value.toString();
+        } else {
+            return stringNumbur
+        }
+    }
+
+    fun containsNumber(stringNumbur: String, valeur: StringBuilder): StringBuilder {
+
+        val stringLength = (stringNumbur.length - 1)
+        for (i in 0..stringLength) {
+            when (stringNumbur[i]) {
+                trois -> valeur.append(Gbsu)
+                cinq -> valeur.append(Ftb)
+                sept -> valeur.append(Lai)
+            }
+        }
+        return valeur;
     }
 
 }
--- gbsuftblai-back/src/test/kotlin/com/kata/gbsuftblai/GbsuFtbLaiApplicationTests.kt
+++ gbsuftblai-back/src/test/kotlin/com/kata/gbsuftblai/GbsuFtbLaiApplicationTests.kt
@@ -1,13 +1,48 @@
 package com.kata.gbsuftblai
 
+import com.kata.gbsuftblai.services.GbsuFtbLaiService
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.junit.Assert.*
 
 @SpringBootTest
-class GbsuFtbLaiApplicationTests {
+class GbsuFtbLaiApplicationTests (@Autowired val restTemplate: GbsuFtbLaiService){
 
     @Test
     fun contextLoads() {
+        var result:String;
+
+        result = restTemplate.convertNumber(1);
+        assertEquals(result,"1");
+
+        result = restTemplate.convertNumber(3);
+        assertEquals(result,"GbsuGbsu");
+
+        result = restTemplate.convertNumber(5);
+        assertEquals(result,"FtbFtb");
+
+        result = restTemplate.convertNumber(7);
+        assertEquals(result,"Lai");
+
+        result = restTemplate.convertNumber(9);
+        assertEquals(result,"Gbsu");
+
+        result = restTemplate.convertNumber(51);
+        assertEquals(result,"GbsuFtb");
+
+        result = restTemplate.convertNumber(53);
+        assertEquals(result,"FtbGbsu");
+
+        result = restTemplate.convertNumber(33);
+        assertEquals(result,"GbsuGbsuGbsu");
+
+        result = restTemplate.convertNumber(27);
+        assertEquals(result,"GbsuLai");
+
+        result = restTemplate.convertNumber(15);
+        assertEquals(result,"GbsuFtbFtb");
+
     }
 
 }
